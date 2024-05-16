553a06efb58d6b76f3b989785af65fd35933fea3
Sveva Azzolini <150719345+AzzoliniS@users.noreply.github.com>
2024-05-16 20:07:04 +0000

    sistemato database

--- 10_maggio_database/index.js
+++ 10_maggio_database/index.js
@@ -2,10 +2,10 @@ const express = require('express');     //carico la libreria
 const mysql = require('mysql2');        //carico la libreria mysql2, successiva a mysql e migliorata
 const bodyParser = require('body-parser');  //per elaborare il body della richiesta http e restituire un oggetto con i dati presenti
 
-const service = express();  //creo un servizio con express
+const app = express();  //creo un servizio con express
 
-service.use(bodyParser.json());     //middleware body-parser per elaborare i dati del body della richiesta HTTP in formato json
-service.use(bodyParser.urlencoded({ extended: false }));    //configura il body-parser per elaborare i dati contenuti nel body della richiesta HTTP in formato HTML Form
+app.use(bodyParser.json());     //middleware body-parser per elaborare i dati del body della richiesta HTTP in formato json
+app.use(bodyParser.urlencoded({ extended: false }));    //configura il body-parser per elaborare i dati contenuti nel body della richiesta HTTP in formato HTML Form
 
 // Definisco un oggetto con i parametri di connessione al server MYSQL (utilizzata dalla libreria mysql2 per stabilire la connessione)
 const parametriConnessioneMysqlServer = {
@@ -16,12 +16,12 @@ const parametriConnessioneMysqlServer = {
 }
 
 // Imposto il servizio in modo che sia in grado di rispondere alle richieste in arrivo dalla rete.
-service.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/home.html');
 });
 
 //Mi permette di restituire tutti gli utenti presenti nella tabella
-service.get('/utenti', (req, res) => {
+app.get('/utenti', (req, res) => {
     const connessione = mysql.createConnection(parametriConnessioneMysqlServer);    // Creo una connessione con il server MYSQL
     const SQLQuery = 'SELECT * FROM utenti';    // Definisco la stringa SQL da eseguire
 
@@ -38,7 +38,7 @@ service.get('/utenti', (req, res) => {
 })
 
 //Mi restituisce l'utente con id indicato nel parametro
-service.get('/utenti/:id', (req, res) => {
+app.get('/utenti/:id', (req, res) => {
     let id = req.params.id;   //Recupero l'id presente nella URL della richiesta
     const connessione = mysql.createConnection(parametriConnessioneMysqlServer);    // Creo una connessione con il server MYSQL
     const SQLQuery = 'SELECT * FROM utenti WHERE id = ?';       // Definisco la stringa SQL da eseguire, il ? verrà poi sostituito nel metodo query
@@ -56,109 +56,68 @@ service.get('/utenti/:id', (req, res) => {
 })
 
 
-// Il metodo consente di aggiungere un nuovo record alla tabella utenti
-// Nome e cognome devono essere inclusi nel body della richiesta HTTP
-// Il verbo HTTP usato è POST: secondo lo standard RESTFUL CRUD
-service.post('/utenti', (richiesta, risposta) => {
-    // Creo un array con i dati da inserire nella tabella. la proprietà body viene messa a disposizione dalla libreria body-parser
-    let datiUtente = [richiesta.body.nome, richiesta.body.cognome];
-    // Creo una connessione con il server MYSQL usando i parametri di connessione precedentemente definiti
+// POST: Aggiungo un nuovo record alla tabella utenti (nome e cognome nel body della richiesta)
+app.post('/utenti', (req, res) => {
+    let datiUtente = [req.body.nome, req.body.cognome]; // Creo un array con i dati da inserire nella tabella
+    const {nome, cognome} = req.body;
+
     const connessione = mysql.createConnection(parametriConnessioneMysqlServer);
-    // Definisco la stringa SQL da eseguire
-    // ATTENZIONE: nomi delle tabelle e dei campi sono case-sensitive
-    // Il punto di domanda verrà sostituito con il valore del secondo parametro del metodo query
-    const SQLQuery = 'INSERT INTO utenti (nome, cognome) VALUES (?, ?)';
-    // Effettuo la query. L'esecuzione avviene in modo asincrono, quindi devo definire la funzione dei callback da eseguire
-    // al recepimento dei dati o di un messaggio di errore
-    connessione.query(SQLQuery, datiUtente, (data, error) => {
-        // Controllo se ci sono stati errori
+    const SQLQuery = 'INSERT INTO utenti (nome, cognome) VALUES (?, ?)';    //SQL da eseguire, ? sostituito con secondo parametro del metodo query
+
+    connessione.query(SQLQuery, datiUtente, (error, data) => {
         if (!error) {
-            // Se non ci sono stati errori, chiudo la connessione con il server mysql (anche questo metodo è asincrono,
-            // quindi imposto una funzione di callback vuota)
             connessione.end(() => {});
-            // Invio i dati al client in formato json
-            risposta.json(data);
+            res.json(data);     //se non ci sono stati errori invio i dati al client in formato json
         }
         else {
-            // Se non ci sono stati errori, chiudo la connessione con il server mysql (anche questo metodo è asincrono,
-            // quindi imposto una funzione di callback vuota)
             connessione.end(() => {});
-            // Invio al client il messaggio di errore con uno status HTTP 500: Internal server error
-            risposta.status(500).send(error);
+            res.status(500).send(error);    //status HTTP 500: Internal server error
         }
     })
 })
 
-// Il metodo consente di eliminare l'utente con id = :id (parametro che dovrà essere sostituito conl'id dell'utente che si vuole ottenere)
-service.delete('/utenti/:id', (richiesta, risposta) => {
-    // Recupero l'id presente nella URL della richiesta
-    let id = richiesta.params.id;
-    // Creo una connessione con il server MYSQL usando i parametri di connessione precedentemente definiti
+// DELETE: eliminare l'utente con id = :id
+app.delete('/utenti/:id', (req, res) => {
+    let id = req.params.id;   // Recupero l'id presente nella URL della richiesta
+
     const connessione = mysql.createConnection(parametriConnessioneMysqlServer);
-    // Definisco la stringa SQL da eseguire
-    // ATTENZIONE: nomi delle tabelle e dei campi sono case-sensitive
-    // Il punto di domanda verrà sostituito con il valore del secondo parametro del metodo query
     const SQLQuery = 'DELETE FROM utenti WHERE id = ?';
-    // Effettuo la query. L'esecuzione avviene in modo asincrono, quindi devo definire la funzione dei callback da eseguire
-    // al recepimento dei dati o di un messaggio di errore
-    connessione.query(SQLQuery, id, (data, error) => {
-        // Controllo se ci sono stati errori
+
+    connessione.query(SQLQuery, id, (error, data) => {
         if (!error) {
-            // Se non ci sono stati errori, chiudo la connessione con il server mysql (anche questo metodo è asincrono,
-            // quindi imposto una funzione di callback vuota)
             connessione.end(() => {});
-            // Invio i dati al client in formato json
-            risposta.json(data);
+            res.json(data);    // Se non ci sono stati errori invio i dati al client in formato json
         }
         else {
-            // Se non ci sono stati errori, chiudo la connessione con il server mysql (anche questo metodo è asincrono,
-            // quindi imposto una funzione di callback vuota)
             connessione.end(() => {});
-            // Invio al client il messaggio di errore con uno status HTTP 500: Internal server error
-            risposta.status(500).send(error);
+            res.status(500).send(error);
         }
     })
 })
 
-// Il metodo consente di modificare un record della tabella utenti
-// Nome e cognome devono essere inclusi nel body della richiesta HTTP
-// Il verbo HTTP usato è PUT: secondo lo standard RESTFUL CRUD
-// l'utente da modificare viene identificato con l'id  (parametro che dovrà essere sostituito conl'id dell'utente che si vuole modificare)
-service.put('/utenti/:id', (richiesta, risposta) => {
-    // Recupero l'id presente nella URL della richiesta
-    let id = richiesta.params.id;
-    // Creo un array con i dati da inserire nella tabella. la proprietà body viene messa a disposizione dalla libreria body-parser
-    // E l'id dell'utente da modificare
-    let datiQuery = [richiesta.body.nome, richiesta.body.cognome, id];
-    // Creo una connessione con il server MYSQL usando i parametri di connessione precedentemente definiti
+// PUT: modifico record identificato con l'id indicato. Nome e cognome devono essere inclusi nel body della richiesta
+app.put('/utenti/:id', (req, res) => {
+    let id = req.params.id;
+
+    let datiQuery = [req.body.nome, req.body.cognome, id];  // Creo un array con i dati da inserire nella tabella.
     const connessione = mysql.createConnection(parametriConnessioneMysqlServer);
-    // Definisco la stringa SQL da eseguire
-    // ATTENZIONE: nomi delle tabelle e dei campi sono case-sensitive
-    // Il punto di domanda verrà sostituito con il valore del secondo parametro del metodo query
+
     const SQLQuery = 'UPDATE utenti SET nome = ?, cognome = ? WHERE id = ?';
-    // Effettuo la query. L'esecuzione avviene in modo asincrono, quindi devo definire la funzione dei callback da eseguire
-    // al recepimento dei dati o di un messaggio di errore
-    connessione.query(SQLQuery, datiQuery, (data, error) => {
-        // Controllo se ci sono stati errori
+
+    connessione.query(SQLQuery, datiQuery, (error, data) => {
         if (!error) {
-            // Se non ci sono stati errori, chiudo la connessione con il server mysql (anche questo metodo è asincrono,
-            // quindi imposto una funzione di callback vuota)
             connessione.end(() => {});
-            // Invio i dati al client in formato json
-            risposta.json(data);
+            res.json(data);
         }
         else {
-            // Se non ci sono stati errori, chiudo la connessione con il server mysql (anche questo metodo è asincrono,
-            // quindi imposto una funzione di callback vuota)
             connessione.end(() => {});
-            // Invio al client il messaggio di errore con uno status HTTP 500: Internal server error
-            risposta.status(500).send(error);
+            res.status(500).send(error);
         }
     })
 })
 
 // Per mettere il servizio in ascolto creo un server che prende come valore il servizio in ascolto sulla porta 3000.
 // La funzione di callback viene eseguita all'avvio del server
-const server = service.listen(3000, () => {
+const server = app.listen(3000, () => {
     console.log('Server in ascolto sulla porta 3000...');
 })
--- Database/index.js
+++ Database/index.js
@@ -1,15 +1,28 @@
-const express = require('express');         //libreria express
-const mysql = require('mysql2');            //libreria mysql2
+const express = require('express');     //carico la libreria 
+const mysql = require('mysql2');        //carico la libreria mysql2, successiva a mysql e migliorata
+const bodyParser = require('body-parser');  //per elaborare il body della richiesta http e restituire un oggetto con i dati presenti
 
-const app = express();      //servizio che utilizza express
+const app = express();  //creo un servizio con express
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());     //middleware body-parser per elaborare i dati del body della richiesta HTTP in formato json
+app.use(bodyParser.urlencoded({ extended: false }));    //configura il body-parser per elaborare i dati contenuti nel body della richiesta HTTP in formato HTML Form
 
-//parametri di connessione al server mysql
-const parametriConnessione = {
-    host: "127.0.0.1",
+// Definisco un oggetto con i parametri di connessione al server MYSQL (utilizzata dalla libreria mysql2 per stabilire la connessione)
+const parametriConnessioneMysqlServer = {
+    host: "127.0.0.1",  //port 3306:3306, server esposto sulla rete del server docker
     user: "root",
     password: "root",
     database: "dbapp"
 }
+
+app.get('/', (req, res) => {
+    res.end("Hello world");
+});
+
+
+
+// Per mettere il servizio in ascolto creo un server che prende come valore il servizio in ascolto sulla porta 3000.
+// La funzione di callback viene eseguita all'avvio del server
+const server = app.listen(3001, () => {
+    console.log('Server in ascolto sulla porta 3001...');
+})
